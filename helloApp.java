fe0313e91c765fdf31d03680b6334228be522dd1
Your Name <you@example.com>
2026-03-18 10:37:43 +0530

    helloApp

--- helloApp.java
+++ helloApp.java
@@ -1,10 +1,16 @@
-public class helloApp {
+public class HelloApp {
     public static void main(String[] args) {
 
-        // Assign name using ternary operator
-        String name = (args.length > 0) ? args[0] : "World";
+        String message;
 
-        // Print greeting
-        System.out.println("Hello, " + name + "!");
+     
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        System.out.println(message);
     }
 }
